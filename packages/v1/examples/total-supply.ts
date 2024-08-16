b76bdd9f69f42e90d8faaf18b6384b4f8e7a927a
SeiyaKobayashi <seiya.kobayashi.work@gmail.com>
2024-08-16 23:13:59 +0900

    Add exemplary code: 'total-supply'

---
+++ packages/v1/examples/total-supply.ts
@@ -0,0 +1,14 @@
+import { jpyc } from '.';
+
+async function main() {
+  // Check total supply
+  const totalSupply = await jpyc.totalSupply();
+  console.log(`totalSupply: ${totalSupply}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
