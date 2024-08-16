aff6b8270ec16c95232057a895b108293ee87405
SeiyaKobayashi <seiya.kobayashi.work@gmail.com>
2024-08-16 23:03:26 +0900

    Add util types

---
+++ packages/core/src/types.ts
@@ -0,0 +1,19 @@
+export type Address = `0x${string}`;
+
+export type Endpoint = string;
+
+export type ChainName =
+  | 'local'
+  | 'ethereum'
+  | 'polygon'
+  | 'gnosis'
+  | 'avalanche'
+  | 'astar';
+
+export type NetworkName =
+  | 'mainnet'
+  | 'goerli'
+  | 'sepolia'
+  | 'amoy'
+  | 'chiado'
+  | 'fuji';
