60065a02ce708fff2acf99a84ac2f904468536d1
SeiyaKobayashi <seiya.kobayashi.work@gmail.com>
2024-08-18 21:40:47 +0900

    Call 'initialize' function during deployment

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,3 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 export * from './addresses';
 export * from './chains';
 export * from './types';
--- packages/v1/ignition/modules/proxy.ts
+++ packages/v1/ignition/modules/proxy.ts
@@ -1,13 +1,33 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { Uint8 } from 'soltypes';
+import { encodeFunctionData } from 'viem';
+
+import { JPYC_V2_ABI } from '../../src';
 
 export const proxyModule = buildModule("ProxyModule", (m) => {
-  // Deploy JPYCv2 Contract
+  // Deploy JPYCv2 contract
   const jpycV2Contract = m.contract("FiatTokenV1");
 
-  // Deploy proxy contract
+  // Deploy proxy contract & initialize JPYCv2 contract
+  const encodedInitializationCall = encodeFunctionData({
+    abi: JPYC_V2_ABI,
+    functionName: 'initialize',
+    args: [
+      'JPY Coin',
+      'JPYC',
+      'Yen',
+      Uint8.from('18'),
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    ],
+  });
+
   const proxyContract = m.contract("ERC1967Proxy", [
     jpycV2Contract,
-    "0x",
+    encodedInitializationCall,
   ]);
 
   return { proxyContract };
